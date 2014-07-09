f16e76e62aeb95cea3c5f8867ff211f41cd59185
Nathan W Mogk <nm8911@gmail.com>
2014-07-08 22:08:54 -0700

    Removed unused imports.

--- Loans/src/main/java/com/nwmogk/bukkit/loans/command/LoanBorrowerHandler.java
+++ Loans/src/main/java/com/nwmogk/bukkit/loans/command/LoanBorrowerHandler.java
@@ -45,10 +45,6 @@
 
 package com.nwmogk.bukkit.loans.command;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
 
