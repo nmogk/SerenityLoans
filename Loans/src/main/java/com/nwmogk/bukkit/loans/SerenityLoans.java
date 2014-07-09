0c40770140ecd3a63d6558c8190a848df70d4e16
Nathan W. Mogk <nmogk@email.arizona.edu>
2014-07-09 14:29:29 -0700

    Added an ExecutorService.

--- Loans/src/main/java/com/nwmogk/bukkit/loans/SerenityLoans.java
+++ Loans/src/main/java/com/nwmogk/bukkit/loans/SerenityLoans.java
@@ -58,16 +58,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public final class SerenityLoans extends JavaPlugin {
 	
 	public static Logger log;
 	
-    public EconomyManager econ = null;
-//    public Permission perms = null;
-//    public Chat chat = null;
-    
     // Incrementing these numbers will force a rebuild of the database.
     public static final int dbMajorVersion = 0;
     public static final int dbMinorVersion = 6;
@@ -77,14 +75,19 @@ public final class SerenityLoans extends JavaPlugin {
     public static int debugLevel;
     
     public Connection conn = null;
+    
     public PlayerManager playerManager;
-
+    public EconomyManager econ;
 	public LoanManager loanManager;
 	public OfferManager offerManager;
+	
+	public ExecutorService threads;
     
 	public void onEnable(){
 		
 		log = Logger.getLogger("Minecraft" + getDescription().getName());
+		
+		this.threads = Executors.newCachedThreadPool();
 		
 		this.saveDefaultConfig();
 		plugin = this;
