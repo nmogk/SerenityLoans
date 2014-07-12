a9795dd303f2a541c6b390323accc217b3e030ec
Nathan W Mogk <nm8911@gmail.com>
2014-07-12 10:56:03 -0700

    Updated to reflect name lookups.

--- Loans/src/main/java/com/nwmogk/bukkit/loans/listener/PlayerLoginListener.java
+++ Loans/src/main/java/com/nwmogk/bukkit/loans/listener/PlayerLoginListener.java
@@ -45,6 +45,8 @@ package com.nwmogk.bukkit.loans.listener;
 import java.util.List;
 import java.util.UUID;
 import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -69,7 +71,7 @@ public final class PlayerLoginListener implements Listener {
 	
 	
 	@EventHandler
-	public void onLogin(PlayerJoinEvent evt){
+	public void onLogin(final PlayerJoinEvent evt){
 		
 		if(SerenityLoans.debugLevel >= 2)
 			SerenityLoans.log.info(String.format("[%s] Attempting to add player %s to system.", plugin.getDescription().getName(), evt.getPlayer().getName()));
@@ -81,68 +83,90 @@ public final class PlayerLoginListener implements Listener {
 		if(!evt.getPlayer().hasPermission("serenityloans.loan.borrow"))
 			return;
 		
-		UUID playerID = evt.getPlayer().getUniqueId();
 		
-		Vector<UUID> toCheck = plugin.playerManager.getManagedEntities(playerID);
-		
-		if(toCheck.size() != 0)
-			for(UUID i : toCheck)
-				toCheck.add(i);
-		
-		toCheck.add(0, playerID);
-		
-		
+		plugin.threads.execute(new Runnable(){
 			
-		boolean firstRun = true;
-		
-		for(UUID i : toCheck){
-			
-			List<Loan> loanSet = plugin.loanManager.getLoansWithOutstandingStatements(i);
-			FinancialEntity currentEntity = plugin.playerManager.getFinancialEntity(i);
-			Player recipient = evt.getPlayer();
-			
-			
-			if(loanSet != null){
+			public void run(){
 				
-				recipient.sendMessage(String.format("%s %s an outstanding payment statement!", prfx, firstRun? "You have" : ((FinancialInstitution)currentEntity).getName() + " has"));
-				recipient.sendMessage(String.format("%s Use %s to apply payment.", prfx, firstRun? "/loan": "/crunion"));
-				recipient.sendMessage(String.format("%s Details are given below:", prfx));
+				UUID playerID = evt.getPlayer().getUniqueId();
 				
 				
-				for(Loan theLoan : loanSet){
-
+				Vector<UUID> toCheck = plugin.playerManager.getManagedEntities(playerID);
+				
+				if(toCheck.size() != 0)
+					for(UUID i : toCheck)
+						toCheck.add(i);
+				
+				toCheck.add(0, playerID);
+				
+				
 					
-					if(recipient == null)
-						continue;
-
-					recipient.sendMessage(plugin.loanManager.getPaymentStatement(theLoan.getLoanID()).toString(plugin));
-					recipient.sendMessage(String.format("%s Use %s statement to view this statement again.", prfx, firstRun? "/loan": "/crunion"));
+				boolean firstRun = true;
+				
+				for(UUID i : toCheck){
 					
+					List<Loan> loanSet = plugin.loanManager.getLoansWithOutstandingStatements(i);
+					FinancialEntity currentEntity = plugin.playerManager.getFinancialEntity(i);
+					Player recipient = evt.getPlayer();
+					
+					
+					if(loanSet != null){
+						
+						plugin.scheduleMessage(recipient, String.format("%s %s an outstanding payment statement!", prfx, firstRun? "You have" : ((FinancialInstitution)currentEntity).getName() + " has"));
+						plugin.scheduleMessage(recipient, String.format("%s Use %s to apply payment.", prfx, firstRun? "/loan": "/crunion"));
+						plugin.scheduleMessage(recipient, String.format("%s Details are given below:", prfx));
+						
+						
+						for(Loan theLoan : loanSet){
+
+							
+							if(recipient == null)
+								continue;
+
+							plugin.scheduleMessage(recipient, plugin.loanManager.getPaymentStatement(theLoan.getLoanID()).toString(plugin));
+							plugin.scheduleMessage(recipient, String.format("%s Use %s statement to view this statement again.", prfx, firstRun? "/loan": "/crunion"));
+							
+						}
+					}
+					
+					List<FinancialEntity> offerResults = plugin.offerManager.getOfferSendersTo(i, true);
+					
+					for(FinancialEntity fe : offerResults){
+						
+						String sender = null;
+						try {
+							sender = plugin.playerManager.entityNameLookup(fe);
+						} catch (InterruptedException | ExecutionException | TimeoutException e) {
+							// TODO add message to configuration
+							plugin.scheduleMessage(recipient, prfx + " Problem during name lookup. Try again later.");
+							continue;
+						}
+						
+						if(sender == null)
+							continue;
+						
+						if(firstRun){
+							plugin.scheduleMessage(recipient, prfx + " You have a loan offer from " + sender + "!");
+							plugin.scheduleMessage(recipient, prfx + " Type '/loan viewoffers' to view your offers.");
+						} else {
+							String recipientS = ((FinancialInstitution)currentEntity).getName();
+							plugin.scheduleMessage(recipient, prfx + " " + recipientS + " has a loan offer from " + sender + "!");
+							plugin.scheduleMessage(recipient, prfx + " Type '/crunion viewoffers' to view your offers.");
+						}
+						
+						plugin.offerManager.registerOfferSend(fe.getUserID(), i);
+						
+						
+					}
+					
+					firstRun = false;
 				}
+				
 			}
 			
-			List<FinancialEntity> offerResults = plugin.offerManager.getOfferSendersTo(i, true);
-			
-			for(FinancialEntity fe : offerResults){
-				
-				String sender = plugin.playerManager.entityNameLookup(fe);
-				
-				if(firstRun){
-					evt.getPlayer().sendMessage(prfx + " You have a loan offer from " + sender + "!");
-					evt.getPlayer().sendMessage(prfx + " Type '/loan viewoffers' to view your offers.");
-				} else {
-					String recipientS = ((FinancialInstitution)currentEntity).getName();
-					evt.getPlayer().sendMessage(prfx + " " + recipientS + " has a loan offer from " + sender + "!");
-					evt.getPlayer().sendMessage(prfx + " Type '/crunion viewoffers' to view your offers.");
-				}
-				
-				plugin.offerManager.registerOfferSend(fe.getUserID(), i);
-				
-				
-			}
-			
-			firstRun = false;
-		}
+		});
+		
+		
 			
 			
 		
