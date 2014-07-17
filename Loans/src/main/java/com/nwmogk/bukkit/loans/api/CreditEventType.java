2a60cf37de74efdc03e0a3a8b4ba491451392201
Nathan W. Mogk <nmogk@email.arizona.edu>
2014-07-17 16:11:30 -0700

    Added toString() method.

--- Loans/src/main/java/com/nwmogk/bukkit/loans/api/CreditEventType.java
+++ Loans/src/main/java/com/nwmogk/bukkit/loans/api/CreditEventType.java
@@ -4,4 +4,34 @@ public enum CreditEventType {
 	
 	LOANOPEN, LOANCLOSE, PAID_BALANCE, PAID_MINIMUM, MISSED_PAYMENT, INACTIVITY, CREDIT_UTILIZATION, CREDIT_LIMIT, BANKRUPT, LOAN_MODIFY, OVERPAYMENT, FINAL_PAYMENT;
 	
+	public String toString(){
+		switch(this){
+		case BANKRUPT:
+			return "Bankruptcy";
+		case CREDIT_LIMIT:
+			return "CreditLimitReached";
+		case CREDIT_UTILIZATION:
+			return "CreditUtilization";
+		case FINAL_PAYMENT:
+			return "Payoff";
+		case INACTIVITY:
+			return "Inactivity";
+		case LOANCLOSE:
+			return "LoanCloase";
+		case LOANOPEN:
+			return "LoanStart";
+		case LOAN_MODIFY:
+			return "LoanModified";
+		case MISSED_PAYMENT:
+			return "MissedPayment";
+		case OVERPAYMENT:
+			return "Overpayment";
+		case PAID_BALANCE:
+			return "Payment";
+		case PAID_MINIMUM:
+			return "MinPayment";
+		default:
+			return null;
+		}
+	}
 }
