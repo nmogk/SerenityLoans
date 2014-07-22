7bf6a552698b215763d225f858d42b4cf8f8bcf3
Nathan W. Mogk <nmogk@email.arizona.edu>
2014-07-22 09:24:18 -0700

    Implemented withdraw functionality. Will do nothing currenlty when given string arguments for player name. I need to think of a strategy for this.

--- Loans/src/main/java/net/milkbowl/vault/economy/plugins/Economy_SerenityLoans.java
+++ Loans/src/main/java/net/milkbowl/vault/economy/plugins/Economy_SerenityLoans.java
@@ -1,8 +1,21 @@
 package net.milkbowl.vault.economy.plugins;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.plugin.Plugin;
+
+import com.nwmogk.bukkit.loans.EconomyManager;
+import com.nwmogk.bukkit.loans.SerenityLoans;
+import com.nwmogk.bukkit.loans.api.EconResult;
+import com.nwmogk.bukkit.loans.api.FinancialEntity;
 
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
@@ -10,6 +23,59 @@ import net.milkbowl.vault.economy.EconomyResponse;
 public class Economy_SerenityLoans implements Economy {
 	
 	// TODO Implement all methods
+	
+	private static final Logger log = Logger.getLogger("Minecraft");
+
+	private final String name = "SerenityLoans Economy";
+    private Plugin plugin = null;
+    private EconomyManager economy = null;
+    private SerenityLoans slPlug = null;
+    
+    public class EconomyServerListener implements Listener {
+        Economy_SerenityLoans economy = null;
+
+        public EconomyServerListener(Economy_SerenityLoans economy) {
+            this.economy = economy;
+        }
+
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onPluginEnable(PluginEnableEvent event) {
+            if (economy.economy == null) {
+                Plugin serenityLoans = event.getPlugin();
+
+                if (serenityLoans.getDescription().getName().equals("SerenityLoans")) {
+                	economy.slPlug = (SerenityLoans) serenityLoans;
+                    economy.economy = ((SerenityLoans) serenityLoans).econ;
+                    log.info(String.format("[%s][Economy] %s hooked.", plugin.getDescription().getName(), economy.name));
+                }
+            }
+        }
+
+        @EventHandler(priority = EventPriority.MONITOR)
+        public void onPluginDisable(PluginDisableEvent event) {
+            if (event.getPlugin().getDescription().getName().equals("SerenityLoans")) {
+                economy.economy = null;
+                economy.slPlug = null;
+                log.info(String.format("[%s][Economy] %s unhooked.", plugin.getDescription().getName(), economy.name));
+            }
+            
+        }
+    }
+    
+	public Economy_SerenityLoans(Plugin plugin){
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EconomyServerListener(this), plugin);
+
+		if (economy == null) {
+            Plugin serenityLoans = plugin.getServer().getPluginManager().getPlugin("SerenityLoans");
+            
+            if (serenityLoans != null && serenityLoans.isEnabled()) {
+                slPlug = (SerenityLoans) serenityLoans;
+            	economy = slPlug.econ;
+                log.info(String.format("[%s][Economy] %s hooked.", plugin.getDescription().getName(), name));
+            }
+        }
+	}
 
 	public EconomyResponse bankBalance(String arg0) {
 		return null;
@@ -165,24 +231,31 @@ public class Economy_SerenityLoans implements Economy {
 	}
 
 	public boolean isEnabled() {
-		return false;
+		if(economy == null)
+			return false;
+		
+		return economy.isInitialized();
 	}
 
+	@Deprecated
 	public EconomyResponse withdrawPlayer(String arg0, double arg1) {
 		return null;
 	}
 
-	public EconomyResponse withdrawPlayer(OfflinePlayer arg0, double arg1) {
-		return null;
+	public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount) {
+		FinancialEntity entity = slPlug.playerManager.getFinancialEntity(player.getUniqueId());
+		EconResult result = economy.withdraw(entity, amount);
+		
+		return economy.convertEconResult(result);
 	}
 
-	public EconomyResponse withdrawPlayer(String arg0, String arg1, double arg2) {
-		return null;
+	@Deprecated
+	public EconomyResponse withdrawPlayer(String playerName, String world, double amount) {
+		return withdrawPlayer(playerName, amount);
 	}
 
-	public EconomyResponse withdrawPlayer(OfflinePlayer arg0, String arg1,
-			double arg2) {
-		return null;
+	public EconomyResponse withdrawPlayer(OfflinePlayer player, String world, double amount) {
+		return withdrawPlayer(player, amount);
 	}
 
 	
